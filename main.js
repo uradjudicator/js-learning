35c0b6e9868f971ad9dcc1228b6657bbea3f5d93
uradjudicator <prind0241@gmail.com>
2023-09-09 13:40:08 +0500

    added some parts

--- main.js
+++ main.js
@@ -3,16 +3,36 @@ const input = document.querySelector('.text')
 const inputColor = document.querySelector('.color')
 const pageTitle = document.querySelector('head title')
 const box = document.querySelector('.box')
+const inpTopL = document.querySelector('.inputLeftTop')
+const inpTopR = document.querySelector('.inputRightTop')
+const inpBottomL = document.querySelector('.inputLeftBottom')
+const inpBottomR = document.querySelector('.inputRightBottom')
+const info = document.querySelector('.info')
 
-btn.addEventListener('click', function(){
-    const value = input.value
-    box.style.borderRadius = value
-})
 btn.addEventListener('click', function(){
     const value = inputColor.value
     box.style.background = value
 })
-box.addEventListener('mouseover', function(){
-    box.style.flexDirection = 'column'
-    box.style.gap = '20px'
+
+btn.addEventListener('click', function(){
+    const value = inpTopL.value
+    box.style.borderTopLeftRadius = value
+})
+btn.addEventListener('click', function(){
+    const value = inpTopR.value
+    box.style.borderTopRightRadius = value
+})
+
+btn.addEventListener('click', function(){
+    const value = inpBottomL.value
+    box.style.borderBottomLeftRadius = value
+})
+
+btn.addEventListener('click', function(){
+    const value = inpBottomR.value
+    box.style.borderBottomRightRadius = value
+})
+btn.addEventListener('click', function(){
+    const value = inpBottomR.value && inpBottomL.value && inpTopR.value && inpTopL.value
+    info.innerText = value
 })
